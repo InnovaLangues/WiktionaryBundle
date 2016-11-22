044d110b60c6a1838a46fbeec2df32009a4f1480
arnaudbey <arnaud.bey@gmail.com>
2016-11-22 11:39:44 +0100

    add getRandomDef and cleaing methods

--- Manager/WiktionaryManager.php
+++ Manager/WiktionaryManager.php
@@ -4,23 +4,65 @@ namespace Innova\WiktionaryBundle\Manager;
 
 class WiktionaryManager
 {
-    public function __construct()
+    public function getDefinitions($lemma, $language)
     {
+        $wiktionaryOutput = $this->requestWiktionary($lemma, $language);
+
+        if ($wiktionaryOutput) {
+            if (preg_match('/<span[\s\S]+?id="fr"[\s\S]*?(<ol>[\s\S]*?<\/ol>)/', $wiktionaryOutput, $matches)) {
+                return $matches[1];
+            }
+        }
+
+        return;
     }
 
-    public function getDefinitions($lemma, $language)
+    public function getRandomDefinition($lemma, $language)
+    {
+        $definitions = $this->getDefinitions($lemma, $language);
+
+        if ($definitions) {
+            $definitions = $this->removeExamples($definitions);
+            if (preg_match_all('/<li>(.*?)<\/li>/s', $definitions, $matches)) {
+                $definitions = $matches[1];
+                $key = array_rand($definitions);
+                $definition = $definitions[$key];
+
+                return $definition;
+            }
+        }
+
+        return;
+    }
+
+    private function removeExamples($definitions)
+    {
+        $definitions = preg_replace("/<ul>.*?<\/ul>/s", '', $definitions);
+
+        return $definitions;
+    }
+
+    private function removeLink($definitions)
+    {
+        $definitions = preg_replace("/<a[^>]+>(.*?)<\/a>/s", '$1', $definitions);
+
+        return $definitions;
+    }
+
+    private function requestWiktionary($lemma, $language)
     {
         $url = 'https://'.$language.'.wiktionary.org/wiki/'.$lemma;
         $handle = @fopen($url, 'r');
-        $def = 'Oups...';
+        $wiktionaryOutput = null;
 
         if ($handle) {
             while (!feof($handle)) {
-                $def = stream_get_contents($handle);
+                $wiktionaryOutput = stream_get_contents($handle);
+                $wiktionaryOutput = $this->removeLink($wiktionaryOutput);
             }
             fclose($handle);
         }
 
-        return $def;
+        return $wiktionaryOutput;
     }
 }
